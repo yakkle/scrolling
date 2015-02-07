2fc7d871b911144b492744bb3a5097bb01869ac1
Mark Allison <mark@stylingandroid.com>
2015-02-07 11:55:26 +0000

    Code for Part 1

---
+++ app/src/main/java/com/stylingandroid/smoothscrolling/MainActivity.java
@@ -0,0 +1,43 @@
+package com.stylingandroid.smoothscrolling;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        recyclerView.setAdapter(LargeAdapter.newInstance(this));
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_top) {
+            recyclerView.smoothScrollToPosition(0);
+            return true;
+        } else if (id == R.id.action_bottom) {
+            recyclerView.smoothScrollToPosition(recyclerView.getAdapter().getItemCount());
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
